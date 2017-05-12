900bf1d3d9dd0630c518f6169160c72dfa4c05a2
g15937sa <g15937sa@s30405.tsuda.ac.jp>
2017-05-12 11:54:25 +0900

    まだ課題の途中です。

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -29,14 +29,14 @@ public class SnakeCamelUtil {
 		return new String(sb);
 	}
 	
-	static String capitalize(String s) {
+	public static String capitalize(String s) {
 		char first = s.charAt(0);
 		char upperFirst = Character.toUpperCase(first);
 		String rest = s.substring(1);
 		return upperFirst + rest;
 	}
 
-	static String uncapitalize(String s) {
+	public static String uncapitalize(String s) {
 		char first = s.charAt(0);
 		char lowerFirst = Character.toLowerCase(first);
 		String rest = s.substring(1);
